0939f664db4027ce026d47c7820e5c80a5ce6fbe
raja dewangan <kingsunknown01@gmail.com>
2025-07-31 11:48:27 +0530

    backend

---
+++ servre.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import { connectDB } from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+import sessionRoutes from './routes/sessionRoutes.js';
+import { verifyToken } from './middleware/auth.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use('/api', authRoutes);
+app.use('/api', verifyToken, sessionRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
